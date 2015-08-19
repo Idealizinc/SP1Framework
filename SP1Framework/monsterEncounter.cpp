c5760089df166cee869a923ff9f7e79e0c92ff0b
Slapyousilly <n00b1ass@gmail.com>
2015-08-19 13:38:39 +0800

    Added Conversion.

---
+++ SP1Framework/monsterEncounter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+void Bat();
+void printBat();
+
+int main()
+{
+
+
+}
+
+void Slime()
+{
+
+}
+
+void printSlime()
+{
+    cout <<  "(° ° )";
+}
---
+++ SP1Framework/monsterPrint.cpp
@@ -0,0 +1,15 @@
+#include "game.h"
+#include "Framework\console.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "conio.h"
+
+
+void printSlime();
+
+void printSlime()
+{
+    
+}
